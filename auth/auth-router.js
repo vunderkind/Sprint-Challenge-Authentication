a4bbcf35c8fcd40127b87bac97d7e746cabd7362
mogwai <feelirabs@gmail.com>
2019-11-15 11:02:30 +0100

    implemented login credentialing

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 const UserHelper = require('./auth-model');
 
+//requiring dependencies
+const bcrypt = require('bcryptjs');
+const bcryption = require('../middleware/bcrypt');
+
+
+router.get('/', (req, res) => {
+  UserHelper.getAll()
+  .then(data => {
+    res.status(200).json(data);
+  })
+})
+
 router.post('/register', (req, res) => {
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  credentials.password = hash;
   UserHelper.register(req.body)
   .then(data => {
     res.status(201).json({message: `New user, with username ${req.body.username} has been created! Good luck!`})
@@ -12,7 +27,15 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const {username, password} = req.body;
+  UserHelper.login(username)
+  .then(user => {
+  if(!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({error: `Incorrect Creds`})
+  } else {
+      return res.status(200).json({message: `Welcome, ${user.username}!`})
+  }
+  })
 });
 
 module.exports = router;
